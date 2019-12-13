b82e30ecc04af9743e291466b9940fd0d5e17e6c
Maxime Princelle <maxime@princelle.org>
2019-12-13 12:34:42 +0100

    Add ColocationRessource to Jersey

--- src/main/java/org/princelle/lp1project/JerseyConfiguration.java
+++ src/main/java/org/princelle/lp1project/JerseyConfiguration.java
@@ -3,6 +3,7 @@ package org.princelle.lp1project;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.princelle.lp1project.Routes.ColocationResource;
 import org.princelle.lp1project.Routes.UserResource;
 import org.springframework.stereotype.Component;
 
@@ -11,5 +12,6 @@ import org.springframework.stereotype.Component;
 public class JerseyConfiguration extends ResourceConfig {
 	public JerseyConfiguration() {
 		register(UserResource.class);
+		register(ColocationResource.class);
 	}
 }
