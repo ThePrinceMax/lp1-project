0bc0ad0918d65277adffab6e2cb529536c04fbad
Maxime Princelle <maxime.princelle@etu.unistra.fr>
2019-12-19 09:28:01 +0100

    Add Utils package

---
+++ src/main/java/org/princelle/lp1project/Utils/StringUtils.java
@@ -0,0 +1,9 @@
+package org.princelle.lp1project.Utils;
+
+public class StringUtils {
+	public static boolean isNullOrEmpty(String str) {
+		if(str == null || str == "")
+			return true;
+		return false;
+	}
+}
