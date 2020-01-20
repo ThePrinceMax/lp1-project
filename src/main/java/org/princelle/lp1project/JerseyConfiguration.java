58c44f8369d8bd9b325ff8ac540e1aae498764db
Maxime Princelle <maxime.princelle@etu.unistra.fr>
2020-01-20 13:56:06 +0100

    Auto-detect Classes in Jersey Configuration

--- src/main/java/org/princelle/lp1project/JerseyConfiguration.java
+++ src/main/java/org/princelle/lp1project/JerseyConfiguration.java
@@ -15,9 +15,20 @@ import java.util.regex.Pattern;
 @Component
 @ApplicationPath("/")
 public class JerseyConfiguration extends ResourceConfig {
-	public JerseyConfiguration() {
-		register(ColocationResource.class);
-		register(PersonResource.class);
-		register(TaskResource.class);
+	public JerseyConfiguration() throws ClassNotFoundException {
+		// Creating Scanner and disabling default filters
+		final ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
+
+		// Add filter that matches all the classes that we want to import.
+		provider.addIncludeFilter(new RegexPatternTypeFilter(Pattern.compile(".*")));
+
+		// Get all the classes defined in specific package
+		final Set<BeanDefinition> classes = provider.findCandidateComponents("org.princelle.lp1project.Routes");
+
+		// Loading each classes from BeanDefinition instance
+		for (BeanDefinition bean: classes) {
+			Class<?> the_class = Class.forName(bean.getBeanClassName());
+			register(the_class); // Register the Resource class for JerseyConfiguration
+		}
 	}
 }
