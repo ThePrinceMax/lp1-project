8f4bda6dfb8825ff4f4f8cd83df9516e5aee6fe8
Maxime Princelle <maxime.princelle@etu.unistra.fr>
2020-01-22 00:06:31 +0100

    Test Fix Cross Origin

--- src/main/java/org/princelle/lp1project/JerseyConfiguration.java
+++ src/main/java/org/princelle/lp1project/JerseyConfiguration.java
@@ -1,19 +1,19 @@
 package org.princelle.lp1project;
 
-import javax.ws.rs.ApplicationPath;
-
 import org.glassfish.jersey.server.ResourceConfig;
-import org.princelle.lp1project.Routes.*;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+import javax.ws.rs.ApplicationPath;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 @Component
 @ApplicationPath("/")
+@CrossOrigin(origins = "*")
 public class JerseyConfiguration extends ResourceConfig {
 	public JerseyConfiguration() throws ClassNotFoundException {
 		// Creating Scanner and disabling default filters
